fa287554f15b1a619e2b859a1939c30250be3d2c
sakethshetty <sakethshetty3412@gmail.com>
2024-07-28 00:36:02 +0530

    added login for admin and trainer

--- backend/app/controllers/auth_controller/signup.py
+++
@@ -1,5 +0,0 @@
-from fastapi import Request
-
-async def signup(signup: dict, requests: Request):
-    result = requests.app.database['trainer'].insert_one(signup.dict())
-    return {"status": "success", "inserted_id": str(result.inserted_id)}
--- backend/app/routes/auth_router.py
+++ backend/app/routes/auth_router.py
@@ -1,18 +1,11 @@
 from fastapi import APIRouter
 from controllers.auth_controller import login as login_controller
-from controllers.auth_controller import signup as signup_controller
 from validation_models.models import LoginRequest
-from validation_models.models import Trainer
 from fastapi import Request
 
 router = APIRouter()
 
-@router.get("/login")
+@router.post("/login")
 async def login(login: LoginRequest, requests: Request):
     data = await login_controller.login(login, requests)
-    return data
-
-@router.get("/signup")
-async def login(signup: Trainer, requests: Request):
-    data = await signup_controller.signup(signup, requests)
     return data
