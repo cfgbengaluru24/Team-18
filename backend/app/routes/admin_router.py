9ac51b9dd95cddb87acfa60283eca6d18e659bbb
sakethshetty <sakethshetty3412@gmail.com>
2024-07-27 23:02:55 +0530

    added trainer in camp endpoint

--- backend/app/routes/admin_router.py
+++ backend/app/routes/admin_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from controllers.admin_controller.camp_on_weekend import get_camp
-from validation_models.models import Weekend 
+from controllers.admin_controller.trainer_in_camp import get_trainers
+from validation_models.models import Weekend, Camp
 from fastapi import Request
 
 router = APIRouter()
@@ -8,4 +9,9 @@ router = APIRouter()
 @router.post("/camp_on_weekend")
 async def camp_on_weekend(weekend : Weekend, requests: Request):
     data = await get_camp(weekend, requests)
+    return data
+
+@router.post("/trainer_in_camp")
+async def trainer_in_camp(camp : Camp, requests: Request):
+    data = await get_trainers(camp, requests)
     return data
